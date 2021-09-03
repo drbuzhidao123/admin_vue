bfb22bbe17b1efade5984545018c2320abfa1cd0
maxuewei <drbuzhidao123@163.com>
2021-09-03 11:07:17 +0800

    环境封装配置修改

--- src/main.js
+++ src/main.js
@@ -4,11 +4,6 @@ import router from "./router";
 import store from "./store";
 import { ElButton } from "element-plus";
 import "element-plus/dist/index.css";
-import axios from "axios";
-import config from "./config";
 
-axios.get(config.mockApi + "/login").then((res) => {
-  console.log(res);
-});
 const app = createApp(App);
 app.use(store).use(router).use(ElButton).mount("#app");
