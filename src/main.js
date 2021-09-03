d8cb73237c078ab5cf25d56dd735590aa768b11e
maxuewei <drbuzhidao123@163.com>
2021-09-03 15:14:34 +0800

    axios的二次封装

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import { ElButton } from "element-plus";
 import "element-plus/dist/index.css";
+import request from "./util/request";
 
 const app = createApp(App);
+app.config.globalProperties.$request = request;
 app.use(store).use(router).use(ElButton).mount("#app");
